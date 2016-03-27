d5bb8bc5e9cd34430940ea04ae9d8d12a38d9392
BigDipper7 <violiniselegant@gmail.com>
2016-03-28 00:25:57 +0800

    add implements of all four fragments

--- app/src/main/java/com/eva/me/tongxinborrow/fragment/BorrowFragment.java
+++ app/src/main/java/com/eva/me/tongxinborrow/fragment/BorrowFragment.java
@@ -1,7 +1,38 @@
 package com.eva.me.tongxinborrow.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eva.me.tongxinborrow.R;
+
 /**
  * Created by eva on 16-3-27.
  */
-public class BorrowFragment {
+public class BorrowFragment extends BaseFragment {
+    private View mRootView;
+    private Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getContext();
+        mRootView = LayoutInflater.from(mContext).inflate(R.layout.fragment_borrow,
+                (ViewGroup) getActivity().findViewById(R.id.main_content), false);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup p = (ViewGroup) mRootView.getParent();
+
+        if (p != null) {
+            p.removeAllViewsInLayout();
+        }
+        return mRootView;
+    }
 }
